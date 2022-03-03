d617ad5d66523eebc305b53d5d53a833d3b70619
arronblak <mitusov.v@list.ru>
2022-03-03 16:38:36 +0300

    практика

--- сервер/app.js
+++
@@ -1,7 +0,0 @@
-
-var fs = require("fs");
-
-setInterval(function(){
-    fs.appendFile("app.log");
-
-};
---
+++ сервер/постройте маршрутизацию.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const requestHandler = (request, response) => {
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+
+    if (request.url === "/error" || request.url === "/") {
+        response.write("<h2>error</h2>");
+    } else if (request.url == "/error2") {
+        response.write("<h2>error2</h2>");
+    } else {
+        response.write("<h2>Not found</h2>");
+    }
+    response.end();
+};
+http.createServer(requestHandler).listen(9000)
