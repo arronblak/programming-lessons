86c60b52974bbab19ea9a99212f871bc088cafde
arronblak <mitusov.v@list.ru>
2022-06-08 13:37:25 +0300

    main

--- сервер/express-api/14.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const port = 3001;
-const server = http.createServer((request, response) => {
-  response.end('Hello, server!')
-})
-server.listen(port, (error) => {
-  if (error) return console.log(`Error: ${error}`);
-  console.log(`Server is listening on port ${port}`)
-})
